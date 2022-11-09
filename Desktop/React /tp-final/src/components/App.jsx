f5bc2e0a4ec91a253d8a9e8b5571de86f271cb9c
marianarubino <rubino.mariana.trabajo@gmail.com>
2022-11-09 16:23:14 -0300

    hasta clase7

--- Desktop/React /tp-final/src/components/App.jsx
+++ Desktop/React /tp-final/src/components/App.jsx
@@ -3,17 +3,33 @@ import FormBusqueda from './FormBusqueda/FormBusqueda';
 import Navbar from './Navbar/Navbar';
 import ItemListContainer from './ItemListContainer/ItemListContainer';
 import CartWidget from './CartWidget/CartWidget';
+import ItemCount from './ItemCount/ItemCount';
+import Home from './Home/Home';
+import Dolar from './Dolar/Dolar';
+import Producto from './Producto/Producto';
+import Carrito from './Carrito/Carrito';
+
 const App = () => {
     return (
         <>
         <Navbar/>
-        <ItemListContainer greeting ={"greeting"} />
+        <Home/>
+        <Producto/>
+        <Carrito/>
+        <ItemCount/>
         <CartWidget/>
+        <ItemListContainer/>
         </>
 
     );
 }
 
+
 export default App;
 
-/* <FormBusqueda busqueda ={"Login"} /> */
+/* 
+<ItemCount/>     ej, clase 5 
+<FormBusqueda busqueda ={"Login"} />   ejemplo de la clase 4
+<CartWidget/>
+<ItemListContainer greeting ={"greeting"} />    ej. clase 
+<Dolar/> .  ejemplo consumo API clase 7*/
---
+++ Desktop/React /tp-final/src/components/Home/Home.jsx
@@ -0,0 +1,41 @@
+import {useState, useEffect} from 'react';
+import {consultarBDD} from '../../assets/funciones.js'
+
+const Home = () => {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        consultarBDD().then(prod => setProductos(prod))
+    },[]);
+
+
+    return (
+        <div className='row'>
+            {productos}  
+        </div>
+    );
+}
+
+export default Home;
+
+
+
+
+/*    const consultarBBD = () =>{
+        fetch ('./json/productos.json')
+        .then (promise => promise.json())
+        .then (productos => {
+            const cardProductos = productos.map(prod =>
+                )
+        } 
+            
+            console.log(productos))
+
+
+    }
+consultarBBD() // llamo a que se ejecute la fx.
+
+/* dentro de fetch: ./ salta directamente a carpeta public  
+    el primer.then convierte los datos de la promesa a archivo json, que son objetos . y cuando termine esa conversion, le pido que me los muestre en consola con el segundo.then 
+*/
+ //el row es para q me muestre los prod en fila en la pag.
